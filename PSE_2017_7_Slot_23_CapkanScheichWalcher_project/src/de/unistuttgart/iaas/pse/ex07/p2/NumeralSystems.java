0d221007e4f55d03765fb6617402e7064d33e74e
Florian <33514818+Acaledus@users.noreply.github.com>
2017-12-21 14:20:45 +0100

    Zwischenstand

--- PSE_2017_7_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex07/p2/NumeralSystems.java
+++ PSE_2017_7_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex07/p2/NumeralSystems.java
@@ -1,12 +1,73 @@
 package de.unistuttgart.iaas.pse.ex07.p2;
 
+import java.util.Scanner;
+
 /**
  * Provides methods for converting numbers between their binary, decimal, and
- * hexadecimal representation. Provides a simple text interface to this
+ * hexadecimal representation. Provides a simple text interface to these
  * conversion methods.
  */
 public class NumeralSystems {
-
-	// TODO Übungsblatt 7, Aufgabe 2
+	public static int dezToBin (int z) {
+		int result = 0;
+		int x = z;
+		for(int i = 0; i <= (int)Math.sqrt(z); i++) {
+			result += Math.pow(10, i) * (x % 2);
+			x /= 2;
+		}
+		return result;
+	}
+	public static String dezToHex (int z) {
+		if(z < 16) {
+			switch (z) {
+			case 10:
+				return "A";
+			case 11:
+				return "B";
+			case 12:
+				return "C";
+			case 13:
+				return "D";
+			case 14:
+				return "E";
+			case 15:
+				return "F";
+			default:
+				String x = String.valueOf(z);
+				return x;
+			}
+		} else {
+			return (dezToHex(z/16)) + (dezToHex(z%16));
+		}
+	}
+	public static String binToHex (int z) {
+		return dezToHex(binToDez(z));
+	}
+	public static int binToDez (int z) {
+		int result = 0;
+		int i = 0;
+		while(z != 0) {
+			result += (z % 10) * (int)Math.pow(2, i);
+			i++;
+			z /= 10;
+		}
+		return result;
+	}
+	public static int hexToBin (String z) {
+		return dezToBin(hexToDez(z));
+	}
+	public static int hexToDez (String z) {
+		return 0;
+	}
+	public static void main(String[] args) {
+		boolean menu = true;
+		Scanner sc = new Scanner(System.in);
+		while (menu) {
+			System.out.println("Bitte geben sie die Basis des gewünschten Zahlensystems an.");
+			int i = sc.nextInt();
+			
+		}
+		sc.close();
+	}
 
 }
